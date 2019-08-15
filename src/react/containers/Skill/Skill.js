5a77a9afd66873a7c621ef7f29af1c0cbb48a695
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-08-14 23:42:02 -0500

    Fixing not showed data for SSR.

--- src/react/containers/Skill/Skill.js
+++ src/react/containers/Skill/Skill.js
@@ -1,5 +1,4 @@
 import React, {Fragment, useContext, useEffect, useState} from 'react';
-import {Redirect, Switch} from 'react-router';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
@@ -11,7 +10,6 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import {getRandomString} from '../../shared/utility';
 import SkillEdit from '../SkillEdit/SkillEdit';
 import {deleteSkill, getSkillsByPanes} from '../../shared/api';
-import {BrowserRouter} from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -37,18 +35,18 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 const Skill = props => {
+    const path = props.history.location.pathname.split('/').pop();
     const context = useContext(State);
     const [skillToEdit, setSkillToEdit] = useState(null);
-    const [selectedPath, setSelectedPath] = useState(null);
+    const [selectedPath, setSelectedPath] = useState(path);
     const classes = useStyles();
     const skills = [];
     useEffect(() => {
-        if (props && props.history && props.history.location && props.history.location.pathname) {
-            setSelectedPath(props.history.location.pathname.split('/').pop());
+        if (selectedPath !== path) {
+            setSelectedPath(path);
             setSkillToEdit(null);
         }
     }, [props.history.location.pathname]);
-
 
     let content = <Spinner/>;
 
@@ -72,59 +70,57 @@ const Skill = props => {
         context.setData(dataToBeUpdated);
     };
 
-    if (context) {
-        let adminOptions = null;
-        skills.push(...context.data.rawData);
-        const selectedSkill = skills.find(el => el.id === selectedPath);
-        if (selectedSkill) {
-            if (context.data.authorized) {
-                adminOptions = <Fragment>
-                    <div className={classes.container}>
-                        <Button
-                            disabled={(skillToEdit !== null)}
-                            onClick={() => onSetSkillEdit(selectedSkill)}
-                            className={classes.button}
-                            variant="contained"
-                            color={'primary'}
-                        >Edit</Button>
-                        <Button className={classes.button} variant="contained" color={'default'}>New</Button>
-                        <Button
-                            className={classes.button}
-                            variant="contained"
-                            color={'secondary'}
-                            onClick={() => onDeleteSkill(selectedSkill.id)}
-                        >Delete</Button>
-                    </div>
-                </Fragment>;
-            }
-            content =
-                <Fragment>
-                    {adminOptions}
-                    {skillToEdit}
-                    <Grid container justify="center">
-                        <Typography variant="h5" style={{
-                            flexGrow: 1, textAlign: 'center', marginTop: 28,
-                        }}>
-                            {selectedSkill.name}
-                        </Typography>
-                        <Avatar
-                            alt="Skill logo"
-                            src={selectedSkill.logo}
-                            style={{margin: 5, width: 70, height: 70}}/>
-                    </Grid>
-                    <Typography variant="subtitle1">
-                        {selectedSkill.description}
-                    </Typography>
-
-                    <Grid container justify={'space-evenly'}>
-                        {selectedSkill.projects.map(project =>
-                            <Grid item key={project.id + '_' + getRandomString(99)}>
-                                <Project project={project}/>
-                            </Grid>,
-                        )}
-                    </Grid>
-                </Fragment>;
+    let adminOptions = null;
+    skills.push(...context.data.rawData);
+    const selectedSkill = skills.find(el => el.id === selectedPath);
+    if (selectedSkill) {
+        if (context.data.authorized) {
+            adminOptions = <Fragment>
+                <div className={classes.container}>
+                    <Button
+                        disabled={(skillToEdit !== null)}
+                        onClick={() => onSetSkillEdit(selectedSkill)}
+                        className={classes.button}
+                        variant="contained"
+                        color={'primary'}
+                    >Edit</Button>
+                    <Button className={classes.button} variant="contained" color={'default'}>New</Button>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color={'secondary'}
+                        onClick={() => onDeleteSkill(selectedSkill.id)}
+                    >Delete</Button>
+                </div>
+            </Fragment>;
         }
+        content =
+            <Fragment>
+                {adminOptions}
+                {skillToEdit}
+                <Grid container justify="center">
+                    <Typography variant="h5" style={{
+                        flexGrow: 1, textAlign: 'center', marginTop: 28,
+                    }}>
+                        {selectedSkill.name}
+                    </Typography>
+                    <Avatar
+                        alt="Skill logo"
+                        src={selectedSkill.logo}
+                        style={{margin: 5, width: 70, height: 70}}/>
+                </Grid>
+                <Typography variant="subtitle1">
+                    {selectedSkill.description}
+                </Typography>
+
+                <Grid container justify={'space-evenly'}>
+                    {selectedSkill.projects.map(project =>
+                        <Grid item key={project.id + '_' + getRandomString(99)}>
+                            <Project project={project}/>
+                        </Grid>,
+                    )}
+                </Grid>
+            </Fragment>;
     }
 
     return (<Fragment>{content}</Fragment>);
